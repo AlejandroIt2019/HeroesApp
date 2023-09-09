d0df668ec16f4f6e483e8024f741f7081e4d2149
Alejandro It <alejandro.it2019v@gmail.com>
2023-09-09 07:06:58 -0300

    add villanpage

--- test/auth/context/authReducer.test.js
+++ test/auth/context/authReducer.test.js
@@ -1,19 +1,39 @@
 import { authReducer } from "../../../src/auth/context/authReducer"
 import { types } from "../../../src/auth/types/types"
 
-describe('first', () => { 
-    test('verify login', () => { 
-        const state = authReducer({logged: false}, {})
-        expect(state).toEqual({logged: false})
+describe('first', () => {
+    test('test logged', () => {
+        const state = authReducer({ logged: false }, {})
+        expect(state).toEqual({ logged: false })
     })
-    test('verificar el login', () => { 
+    test('test logged true', () => {
+        const state = authReducer({ logged: true }, {})
+        expect(state).toEqual({ logged: true })
+    })
+    test('test login with action', () => {
         const action = {
             type: types.login,
-            payload: {
+            user: {
                 id: '123',
                 user: 'ale'
             }
         }
-        
+        const state = authReducer({ logged: false }, action)
+        expect(state).toEqual({
+            logged: true,
+            user: action.payload
+        })
+
     })
- })
+    test('test logout', () => {
+        const action = {
+            type: types.logout,
+
+        }
+        const state = authReducer({ logged: true }, action)
+        expect(state).toEqual({
+            logged: false
+        })
+
+    })
+})
